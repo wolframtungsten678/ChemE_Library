55da98f6874feea84856ad36adc8fa045bd8f021
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2025-10-20 07:50:31 -0400

    Added IAPWS 97 Constants

---
+++ src/core/constants.zig
@@ -0,0 +1,5 @@
+const units = @import("units.zig");
+
+pub const WATER_CRITICAL_TEMPERATURE: units.K = units.K.init(647.096);
+pub const WATER_CRITICAL_PRESSURE: units.Pa = units.Pa.init(22.06e6);
+pub const WATER_GAS_CONSTANT: units.JPerKgK = units.JPerKgK.init(461.526);
